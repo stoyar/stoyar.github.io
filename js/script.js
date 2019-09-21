9db7f52c65836d0de6ab775d1fef8a40b925d214
Yaroslav Marynchuk <46286123+stoyar@users.noreply.github.com>
2019-09-21 23:28:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 
-let dx = 2;
-let dy = 2;
+let dx = 3;
+let dy = 3;
 let ballRadius = 60;
 let x = 0;
 let y = 0;
 let z = ballRadius;
 let balls = 20;
+let ballColor = '#0095dd';
+
+let paddleHeight = 20;
+let paddleWidth = 100;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+function randomColor() {
+    let a = Math.floor(Number(Math.random()*255));
+    let b = Math.floor(Number(Math.random()*255));
+    let c = Math.floor(Number(Math.random()*255));
+    ballColor = `rgb(${a}, ${b}, ${c})`
+}
 
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = '#0095dd';
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.stroke();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0070cc';
+    ctx.fill();
     ctx.closePath();
 }
 
@@ -24,15 +46,44 @@ function draw() {
     if(z > 0) {
         z--;
     } else {
-    if(dy + y < ballRadius || dy + y > canvas.height-ballRadius) {
+        if(dy + y < ballRadius || dy + y > canvas.height-ballRadius) {
         dy = -dy;
-    }
-    if(dx + x > canvas.width-ballRadius || dx + x < ballRadius) {
+        randomColor();
+        }
+        if(dx + x > canvas.width-ballRadius || dx + x < ballRadius) {
         dx = -dx;
+        randomColor();
+        }
     }
-}
     x += dx;
     y += dy;
     
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 5;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 5;
+    }
+
 }
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    } else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    } else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
 setInterval(draw, 10);
